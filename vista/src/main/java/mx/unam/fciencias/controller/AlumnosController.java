f842dcac72ceae10fb215e64a59f5900f4a911bb
grojas <memo.onilink@gmail.com>
2014-08-25 15:17:33 -0500

    se agrego controller de alumno

---
+++ vista/src/main/java/mx/unam/fciencias/controller/AlumnosController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mx.unam.fciencias.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import mx.unam.fciencias.model.dto.AlumnoDto;
+
+
+/**
+ *
+ * @author guillermorojas
+ */
+@ManagedBean
+@SessionScoped
+public class AlumnosController {
+    
+    private AlumnoDto alumno;
+    
+    private Collection<AlumnoDto> alumnos;
+    
+    @PostConstruct
+    public void init(){
+        alumno=new AlumnoDto();
+        alumnos=new ArrayList<AlumnoDto>();
+    }
+    
+    public void guardaAlumno(){
+        if(alumnos.contains(alumno)){
+        
+        }else{
+            alumnos.add(alumno);
+            alumno=new AlumnoDto();
+        }
+    }
+    
+    public void borraAlumno(AlumnoDto almn){
+        if(alumnos.contains(almn)){
+            alumnos.remove(almn);
+        }
+        else{
+        
+       }
+    }
+
+    public AlumnoDto getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(AlumnoDto alumno) {
+        this.alumno = alumno;
+    }
+
+    public Collection<AlumnoDto> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(Collection<AlumnoDto> alumnos) {
+        this.alumnos = alumnos;
+    }
+    
+    
+    
+    
+}
