ff3afcbd7cd11fcc6bf4ca845e82e51e7ebe551b
grojas <memo.onilink@gmail.com>
2014-09-22 14:39:57 -0500

    Cambios clase: 25 de agosto 2014

---
+++ vista/src/main/java/mx/unam/fciencias/dao/AbstractDAO.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mx.unam.fciencias.dao;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+
+public abstract class AbstractDAO<T> {
+
+    protected EntityManager em;
+    protected Class<T> clase;
+
+    public AbstractDAO(Class<T> c) {
+        this.em = Persistence.createEntityManagerFactory("cienciasPU").createEntityManager();
+        clase = c;
+    }
+
+    public void create(T t) {
+        em.persist(t);
+    }
+
+    public T find(Long id) {
+        return em.find(clase, id);
+    }
+
+    public T merge(T t) {
+        return em.merge(t);
+    }
+
+    public void delete(Long id) {
+        T t = em.find(clase, id);
+        if (t != null) {
+            em.remove(t);
+        }
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+}
---
+++ vista/src/main/java/mx/unam/fciencias/dao/AlumnoDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mx.unam.fciencias.dao;
+
+import mx.unam.fciencias.model.dto.AlumnoDto;
+
+/**
+ *
+ * @author guillermorojas
+ */
+public class AlumnoDAO extends AbstractDAO<AlumnoDto>{
+
+    public AlumnoDAO(Class<AlumnoDto> c) {
+        super(c);
+    }
+    
+}
